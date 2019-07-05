7915706d71ed4e2b1da62180ee5d2fc787345337
uto-usui <me@uto-usui.com>
2019-07-06 02:52:37 +0900

    ✨ Add sub page

---
+++ pages/sub/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <Wrapper class="page-home">
+    <div class="ph__inner">
+      <h2>
+        this is sub
+      </h2>
+    </div>
+  </Wrapper>
+</template>
+
+<script lang="ts">
+import mixins from 'vue-typed-mixins'
+import Wrapper from '~/components/Wrapper.vue'
+import LazyImage from '~/components/util/LazyImage.vue'
+
+import headMixin from '~/mixins/Head.vue'
+
+// interface data {
+// test: string
+// }
+
+export default mixins(headMixin).extend({
+  components: {
+    Wrapper,
+    LazyImage,
+  },
+  data: () => ({
+    //
+  }),
+  mounted() {
+    this.$dispatch('global/setIsPageReady', true)
+  },
+  updated() {
+    //
+  },
+  beforeDestroy() {
+    this.$dispatch('global/setIsPageReady', false)
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.page-home {
+  //
+}
+
+.ph__inner {
+  padding-right: 10vw;
+  padding-left: 10vw;
+}
+
+.ph__figure {
+  + .ph__figure {
+    margin-top: 10vw;
+  }
+}
+</style>
