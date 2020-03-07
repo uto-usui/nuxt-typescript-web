1e22ef5c5d92d56fbf17fd7804e2f8be304bc904
uto-usui <nattyanoranged@gmail.com>
2020-03-07 14:27:32 +0900

    🔧 Add some component setting for WebStorm - import-resolver.js 🔧

--- import-resolver.js
+++ import-resolver.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-undef */
 import Vue from 'vue'
+import TheKicker from '~/components/atom/TheKicker'
+import PublishDate from '~/components/atom/PublishDate/PublishDate'
+import TheButton from 'index.ts'
+import TheHed from '~/components/atom/TheHed/TheHed'
+import Layout from '~/components/core/grid/Layout'
+import Row from '~/components/core/grid/Row'
+import Col from '~/components/core/grid/Col'
+
+Vue.component('TheKicker', TheKicker)
+Vue.component('PublishDate', PublishDate)
+Vue.component('TheButton', TheButton)
+Vue.component('TheHed', TheHed)
+Vue.component('Col', Col)
+Vue.component('Row', Row)
+Vue.component('Layout', Layout)
 
 Vue.component('Heading1', {})
 Vue.component('LazyImage', {})
 Vue.component('Wrapper', {})
-Vue.component('Col', {})
-Vue.component('Row', {})
-Vue.component('Layout', {})
 
 System.config({
   paths: {
