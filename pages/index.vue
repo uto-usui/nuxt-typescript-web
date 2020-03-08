0cce42fd55171b85fbe25df363e1918bd477c4aa
uto-usui <nattyanoranged@gmail.com>
2020-03-08 14:35:06 +0900

    🚚 Move LazyImage 🚚

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
 <script lang="ts">
 import mixins from 'vue-typed-mixins'
 import Wrapper from '~/components/Wrapper.vue'
-import LazyImage from '~/components/util/LazyImage.vue'
+import LazyImage from '~/components/core/LazyImage/LazyImage'
 import Heading1 from '~/components/Heading1.vue'
 
 import headMixin from '~/mixins/Head.vue'
