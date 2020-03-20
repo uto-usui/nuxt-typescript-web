c78da23f467a92d69bec77f7cb7c2b2b1e530faa
uto-usui <nattyanoranged@gmail.com>
2020-03-21 01:24:54 +0900

    🔧 Update stylelint setting for v-deep - dart-sass 🔧

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,12 @@ module.exports = {
   extends: ['stylelint-config-good-scss', './node_modules/prettier-stylelint/config.js'],
   rules: {
     // 'no-descending-specificity': null,
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep']
+      }
+    ]
   },
 }
+
