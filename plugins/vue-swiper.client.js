ed446285865d4204e0f4aabee59fdc6098d223c8
uto-usui <nattyanoranged@gmail.com>
2020-03-30 10:45:26 +0900

    ⬆️ Update vue-awesome-swiper v4.1.0 ⬆️

--- plugins/vue-swiper.client.js
+++ plugins/vue-swiper.client.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
-if (process.browser) {
-  const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr')
-  Vue.use(VueAwesomeSwiper)
-}
+Vue.use(VueAwesomeSwiper)
