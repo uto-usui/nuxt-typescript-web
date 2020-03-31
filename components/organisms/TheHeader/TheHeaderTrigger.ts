17dde27797482ebe85a178f369e5bba4638c9041
uto-usui <nattyanoranged@gmail.com>
2020-03-31 13:28:14 +0900

    ✨ Add TheHeaderTrigger component ✨

---
+++ components/organisms/TheHeader/TheHeaderTrigger.ts
@@ -0,0 +1,11 @@
+import { defineComponent } from '@vue/composition-api'
+import { Layout, Row, Col } from '@/components/core/grid'
+
+export default defineComponent({
+  name: 'TheHeaderTrigger',
+  components: {
+    Layout,
+    Row,
+    Col,
+  },
+})
---
+++ components/organisms/TheHeader/TheHeaderTrigger.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="the-header-trigger">
+    <div class="tht__inner">
+      menu
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./TheHeaderTrigger.ts"></script>
+
+<style lang="scss" scoped>
+@import '~assets/sass/object/organisms/the-header/trigger/_index.scss';
+</style>
